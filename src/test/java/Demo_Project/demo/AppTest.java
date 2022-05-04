15877f5d90ec70ff4950d36b02c450e29549d156
Brij961 <brijesh.mishra@klearnow.com>
2022-05-04 12:22:52 +0530

    FC

---
+++ src/test/java/Demo_Project/demo/AppTest.java
@@ -0,0 +1,17 @@
+package Demo_Project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+	
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
